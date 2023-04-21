3f2c617165090b7cc9426779a12b3f96ae2b0918
Peat <hendy123@gmx.de>
2023-04-21 23:33:19 +0200

    Initial commit

---
+++ GameJam-Game/Assets/Scripts/Scriptables/ResourceData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "Resource Data", menuName = "Resource Data", order = 0)]
+    public class ResourceData : ScriptableObject
+    {
+        [SerializeField] private float m_startingValue;
+        [SerializeField] private float m_maxValue;
+
+        public float StartingValue => this.m_startingValue;
+        public float MaxValue => this.m_maxValue;
+    }
+}
