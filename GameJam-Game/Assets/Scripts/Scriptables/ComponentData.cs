8148b3756f0bdac4a00b7f8614ef24bc9532eabd
Mihitoko <max-lotz@outlook.de>
2023-04-29 17:58:00 +0200

    Add ComponentData.cs

---
+++ GameJam-Game/Assets/Scripts/Scriptables/ComponentData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "Component Data", menuName = "Data/Player Data", order = 0)]
+    public class ComponentData: ScriptableObject
+    {
+        [SerializeField] public string componentName = "NoName";
+        [SerializeField] public RawImage icon;
+    }
+}
