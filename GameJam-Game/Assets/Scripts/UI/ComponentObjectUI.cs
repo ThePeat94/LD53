e24585c7bef1a58b5dde34b808d6090b952494d8
Peat <hendy123@gmx.de>
2023-05-01 20:37:37 +0200

    added timer to levels fixed R for retry bug added ui for component objects read/write error

---
+++ GameJam-Game/Assets/Scripts/UI/ComponentObjectUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ComponentObjectUI : MonoBehaviour
+    {
+        [SerializeField] private Image m_componentObjectImage;
+        [SerializeField] private ComponentObject m_componentObject;
+
+        private void Awake()
+        {
+            if (this.m_componentObject == null)
+            {
+                this.m_componentObject = this.GetComponentInParent<ComponentObject>();
+            }
+        }
+        
+        private void Start()
+        {
+            this.m_componentObjectImage.sprite = this.m_componentObject.ComponentData.Icon;
+        }
+        
+        private void Update()
+        {
+            this.transform.LookAt(Camera.main.transform);
+        }
+    }
+}
