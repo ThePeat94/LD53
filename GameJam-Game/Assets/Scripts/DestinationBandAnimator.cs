d6452ab72d8ae5b1f43695b090e20308a0b5c5ff
Peat <hendy123@gmx.de>
2023-05-01 06:42:24 +0200

    Integrated some Menu Assets

---
+++ GameJam-Game/Assets/Scripts/DestinationBandAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DestinationBandAnimator : MonoBehaviour
+    {
+        [SerializeField] private Material m_bandMaterial;
+
+        private void Start()
+        {
+            this.m_bandMaterial.DOOffset(new Vector2(5, 0), 1f).SetLoops(-1).SetEase(Ease.Linear);
+        }
+    }
+}
