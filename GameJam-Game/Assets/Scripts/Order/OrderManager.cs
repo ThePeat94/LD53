389b6a61a3788a19e225b848d8af05a6ff6a6936
Peat <hendy123@gmx.de>
2023-05-01 21:29:16 +0200

    Spawn new order faster when no orders are there

--- GameJam-Game/Assets/Scripts/Order/OrderManager.cs
+++ GameJam-Game/Assets/Scripts/Order/OrderManager.cs
@@ -13,20 +13,18 @@ namespace DefaultNamespace.Order
     public class OrderManager : MonoBehaviour
     {
         private EventHandler<PackageOrderChangeEventArgs> m_orderSpawned;
-        // TODO: Subscribe in time manager to this event and cut of time
-        private EventHandler<PackageOrderChangeEventArgs> m_orderExpired; 
-        // TODO: Subscribe in time manager to this event and add time
+        private EventHandler<PackageOrderChangeEventArgs> m_orderExpired;
         private EventHandler<PackageOrderChangeEventArgs> m_orderDelivered;
 
         [SerializeField] private LevelData m_levelData;
         [SerializeField] private GameStateManager m_gameStateManager;
-        
+
         private Queue<PackageOrder> m_currentOrders = new();
 
         private List<OrderData> m_availableOrders;
         private int m_currentOrderSpawnFrameCountdown;
         private int m_maxOrders = 5;
-        
+
         private ComponentEndPoint m_endPoint;
 
         public event EventHandler<PackageOrderChangeEventArgs> OrdersSpawned
@@ -34,37 +32,37 @@ namespace DefaultNamespace.Order
             add => this.m_orderSpawned += value;
             remove => this.m_orderSpawned -= value;
         }
-        
+
         public event EventHandler<PackageOrderChangeEventArgs> OrderExpired
         {
             add => this.m_orderExpired += value;
             remove => this.m_orderExpired -= value;
         }
-        
+
         public event EventHandler<PackageOrderChangeEventArgs> OrderDelivered
         {
             add => this.m_orderDelivered += value;
             remove => this.m_orderDelivered -= value;
         }
-        
+
         public IReadOnlyCollection<PackageOrder> CurrentOrders => this.m_currentOrders;
 
         private void Awake()
         {
             this.m_availableOrders = this.m_levelData.AvailableOrders.ToList();
             this.m_currentOrderSpawnFrameCountdown = this.m_levelData.FirstOrderAfterFrames;
-                
+
             if (this.m_endPoint is null)
             {
                 this.m_endPoint = FindObjectOfType<ComponentEndPoint>();
             }
+
             this.m_endPoint.PackageDelivered += this.OnPackageDelivered;
 
             if (this.m_gameStateManager == null)
             {
                 this.m_gameStateManager = FindObjectOfType<GameStateManager>();
             }
-            
         }
 
 
@@ -72,7 +70,7 @@ namespace DefaultNamespace.Order
         {
             if (this.m_gameStateManager.CurrentState != GameStateManager.State.Playing)
                 return;
-            
+
             this.ExpireOrders();
             this.SpawnNewOrders();
         }
@@ -84,13 +82,18 @@ namespace DefaultNamespace.Order
             this.m_currentOrderSpawnFrameCountdown--;
             if (this.m_currentOrderSpawnFrameCountdown <= 0)
             {
-                Debug.Log("Creating new Order");
-                this.m_currentOrderSpawnFrameCountdown = Random.Range(this.m_levelData.MinFramesForOrderSpawn, this.m_levelData.MaxFramesForOrderSpawn + 1);
-                var rndOrderData = this.m_availableOrders[UnityEngine.Random.Range(0, this.m_availableOrders.Count)];
-                var newPackageOrder = new PackageOrder(rndOrderData);
-                this.m_currentOrders.Enqueue(newPackageOrder);
-                this.m_orderSpawned?.Invoke(this, new PackageOrderChangeEventArgs(newPackageOrder));
+                this.SpawnNewOrder();
             }
+        }
+
+        private void SpawnNewOrder()
+        {
+            Debug.Log("Creating new Order");
+            this.m_currentOrderSpawnFrameCountdown = Random.Range(this.m_levelData.MinFramesForOrderSpawn, this.m_levelData.MaxFramesForOrderSpawn + 1);
+            var rndOrderData = this.m_availableOrders[UnityEngine.Random.Range(0, this.m_availableOrders.Count)];
+            var newPackageOrder = new PackageOrder(rndOrderData);
+            this.m_currentOrders.Enqueue(newPackageOrder);
+            this.m_orderSpawned?.Invoke(this, new PackageOrderChangeEventArgs(newPackageOrder));
         }
 
         private void OnPackageDelivered(object sender, PackageDeliveryEventArgs args)
@@ -102,7 +105,7 @@ namespace DefaultNamespace.Order
         {
             var copiedOrderQueue = new Queue<PackageOrder>(this.m_currentOrders);
             var index = -1;
-            PackageOrder orderWithAll = null; 
+            PackageOrder orderWithAll = null;
             while (copiedOrderQueue.Count > 0)
             {
                 index++;
@@ -112,6 +115,7 @@ namespace DefaultNamespace.Order
                 {
                     continue;
                 }
+
                 foreach (var contained in deliveredPackage.ContainedComponents)
                 {
                     if (!order.OrderData.NeededComponents.Contains(contained))
@@ -139,11 +143,13 @@ namespace DefaultNamespace.Order
             listedOrders.RemoveAt(index);
             this.m_currentOrders = new Queue<PackageOrder>(listedOrders);
             this.m_orderDelivered?.Invoke(this, new PackageOrderChangeEventArgs(orderWithAll));
+
+            if (this.m_currentOrders.Count == 0)
+                this.m_currentOrderSpawnFrameCountdown = Math.Min(this.m_currentOrderSpawnFrameCountdown, 10);
         }
 
         public void ExpireOrders()
         {
-
             var copiedOrders = this.m_currentOrders.ToList();
             var expiredOrders = new List<PackageOrder>();
             var index = 0;
