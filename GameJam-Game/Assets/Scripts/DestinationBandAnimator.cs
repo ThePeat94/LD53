1e259166b62153be97d24e94c2cdf9a5a7a512c4
Hendrik Rakemann <dev@hendrikrakemann.de>
2024-08-29 14:50:07 +0200

    STORY - Adjust animation speed for package band. #128

--- GameJam-Game/Assets/Scripts/DestinationBandAnimator.cs
+++ GameJam-Game/Assets/Scripts/DestinationBandAnimator.cs
@@ -5,10 +5,12 @@ namespace Nidavellir
 {
     public class DestinationBandAnimator : MonoBehaviour
     {
+        [SerializeField] private float m_bandSpeed;
+        
         private void Start()
         {
             var material = this.GetComponent<MeshRenderer>().material;
-            material.DOOffset(new Vector2(-1f, 0), 1f).SetLoops(-1).SetEase(Ease.Linear);
+            material.DOOffset(new Vector2(-1f, 0), this.m_bandSpeed).SetLoops(-1).SetEase(Ease.Linear);
         }
     }
 }
