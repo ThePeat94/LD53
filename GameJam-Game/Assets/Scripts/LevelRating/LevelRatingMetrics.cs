14533c68aacaa40a713e00f54d19c40e66492deb
Peat <hendy123@gmx.de>
2024-09-01 01:57:28 +0200

    TASK - Implement first few classes to calculate a level rating. #146

---
+++ GameJam-Game/Assets/Scripts/LevelRating/LevelRatingMetrics.cs
@@ -0,0 +1,18 @@
+﻿namespace Nidavellir.LevelRating
+{
+    public class LevelRatingMetrics
+    {
+        public int FailedOrders { get; }
+        public int InOrderOrders { get; }
+        public int LeftTimeFrames { get; }
+        public int UnneededOrders { get; }
+
+        public LevelRatingMetrics(int failedOrders, int inOrderOrders, int leftTimeFrames, int unneededOrders)
+        {
+            this.FailedOrders = failedOrders;
+            this.InOrderOrders = inOrderOrders;
+            this.LeftTimeFrames = leftTimeFrames;
+            this.UnneededOrders = unneededOrders;
+        }
+    }
+}
